e4d11b2926e7fd0f6f86c78e33af712b144341be
Sam Rapaport <me@samrapaport.com>
2022-11-22 09:58:21 -0800

    Notify users of new chirps 🤘🏼

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Chirp extends Model
 {
@@ -11,6 +12,10 @@ class Chirp extends Model
 
     protected $fillable = [
         'message'
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 
     public function user()
